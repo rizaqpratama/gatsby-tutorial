8edd67226fe01904059a9ce07052371389d66c28
Rizaq Nanda Pratama <rizaq.pratama@ninjavan.co>
2019-11-07 05:44:04 +0700

    inline styling

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './style.css'
+import { fontSize } from '@material-ui/system'
 
 export const Hero = ({ tagline }) => {
   return (
-    <div>
-      {tagline}
+    <div className="hero-container">
+      <h3 className="hero-tagline">{tagline}</h3>
+      <p style={{ fontSize: '12pt', fontStyle: 'italic' }}>Web developer terpercaya siap membantu anda membuat website dengan react js</p>
     </div>
   )
 }
