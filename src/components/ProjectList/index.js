7e89b665c3ee331a5b701a1f7e48572e7e04aa03
Rizaq Nanda Pratama <rizaq.pratama@ninjavan.co>
2019-11-06 23:01:52 +0700

    remove octokit

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Octokit from '@octokit/rest'
+import axios from 'axios'
 
 
 export const ProjectList = () => {
   const [repos, setRepos] = useState([])
   useEffect(() => {
-    const oc = new Octokit({
-      auth: "token 1d74f70b7fccbe3ddbb6aa4e9f555aaa0b714d45"
+    
+    axios.get('https://api.github.com/users/rizaqpratama/repos')
+    .then((response) => {
+      if(response.status === 200) {
+        if(response.data.length > 5) {
+              setRepos(response.data.slice(0,4))
+            } else {
+              setRepos(response.data)
+            }
+      }
     })
-
-  oc.repos
-  .listForUser({
-    username: 'rizaqpratama'
-  })
-  .then(({ data }) => {
-
-    if(data.length > 5) {
-      setRepos(data.slice(0,4))
-    } else {
-      setRepos(data)
-    }
-  });
     
   },[])
 
@@ -28,7 +24,13 @@ export const ProjectList = () => {
     <div>
       <ul>
       { repos.map(repo => {
-        return(<li key={repo.name}>{repo.name}</li>) 
+        return(<li key={repo.name}>
+          <div>
+            <h4><a href={repo.html_url}>{repo.name}</a></h4>
+            <p>{repo.description}</p>
+          </div>
+
+        </li>) 
       }) }
       </ul>
     </div>
