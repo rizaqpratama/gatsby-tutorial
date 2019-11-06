fe35bd0dde6ee4fe4dd15365ecebb1772138dfbb
Rizaq Nanda Pratama <rizaq.pratama@ninjavan.co>
2019-11-07 06:11:15 +0700

    styled component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './style.css'
 import { fontSize } from '@material-ui/system'
+import { HeroImage } from './styles'
+import imgSource from '../../images/1.png'
 
 export const Hero = ({ tagline }) => {
   return (
     <div className="hero-container">
-      <h3 className="hero-tagline">{tagline}</h3>
-      <p style={{ fontSize: '12pt', fontStyle: 'italic' }}>Web developer terpercaya siap membantu anda membuat website dengan react js</p>
+      <div>
+        <h3 className="hero-tagline">{tagline}</h3>
+        <p style={{ fontSize: '12pt', fontStyle: 'italic' }}>Web developer terpercaya siap membantu anda membuat website dengan react js</p>
+      </div>
+      <HeroImage src={imgSource} />
     </div>
   )
 }
